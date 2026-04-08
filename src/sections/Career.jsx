03fc04b305d75cd5977b12e82f07941c10e78d88
lijkott <elijahkott@gmail.com>
2026-04-08 09:38:36 -0600

    updated jobs and projects

--- src/sections/Career.jsx
+++ src/sections/Career.jsx
@@ -8,6 +8,15 @@ function Career() {
       <div className="career-box">
         <h1 className="Header"><b>Career</b></h1>
         <Jobs
+          companyName={"Kotter Data"}
+          linkedinHandle={"kotter-data"}
+          timeWorked={"May 29 2024 - Present"}
+          description={"I am the COO and CTO of Kotter Data. I do IT work for Kotter Tax and Accounting."}
+          skills={["Leadership", "Business"]}
+          imageUrl={"https://msgsndr-private.storage.googleapis.com/companyPhotos/50107f2e-35f7-49d4-a15c-231e0284161d.png"}
+          altText={"Kotter Data"}
+        />
+        <Jobs
           companyName={"Monroe Mountain Marketing"}
           linkedinHandle={"monroe-mountain-marketing"}
           timeWorked={"Jan 13 2026 - Present"}
@@ -17,13 +26,12 @@ function Career() {
           altText={"Monroe Mountain Marketing"}
         />
         <Jobs
-          companyName={"Kotter Data"}
-          linkedinHandle={"kotter-data"}
-          timeWorked={"May 29 2024 - Present"}
-          description={"I am the COO and CTO of Kotter Data. I do IT work for Kotter Tax and Accounting."}
-          skills={["Leadership", "Business"]}
-          imageUrl={"https://msgsndr-private.storage.googleapis.com/companyPhotos/50107f2e-35f7-49d4-a15c-231e0284161d.png"}
-          altText={"Kotter Data"}
+          companyName={"Kotter Dash"}
+          timeWorked={"Dec 3 2025 - May 22 2026"}
+          description={"I started a Business for an entrepreneurship class. I did a local doordash business where I would deliever food to those a part of Richfield High. I made a few orders and made a profit of 130 dollars. It was a great introduction to the business world."}
+          skills={["Business", "Leadership"]}
+          imageUrl={ }
+          altText={"Kotter Dash Logo"}
         />
       </div>
     </div>
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -8,12 +8,20 @@ function Projects() {
       <div className="projects-box">
         <h1 className="Header"><b>Projects</b></h1>
         <ProjectComp
+          projectName={"First Rust Project"}
+          link={"https://github.com/LijKott/FirstRustProject"}
+          description={"This is an ever changing project that will hold all my learning for rust. I will add to this and change it to reflect what I have learned"}
+          skills={["Rust", "Programming"]}
+          imageUrl={"https://miro.medium.com/0*Eqqrv9zVpH99X726.png"}
+          altText={"Rust Project Image"}
+        />
+        <ProjectComp
           projectName={"Portfolio"}
           link={"https://github.com/LijKott/portfolio"}
           description={"I made a Portfolio so I can look pretty cool."}
           skills={["HTML", "Javascript", "CSS", "React"]}
           imageUrl={"https://i.sstatic.net/IYZ5MrvW.png"}
-          altText={"Portfolio"}
+          altText={"Portfolio image"}
         />
       </div>
     </div>
